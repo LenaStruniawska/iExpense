4b91beb1ea39ed57a88b1119bcf36540c6b75d49
Magdalena Struniawska <mag.sok@onet.pl>
2022-03-16 13:59:16 +0100

    Initial Commit

---
+++ IExpenseProjekt/IExpenseProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseProjektApp.swift
+//  IExpenseProjekt
+//
+//  Created by Slawek on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
